39e64d82e09bd5396ccfb1f605454e2cc63cbd19
Simone Sanfratello <s.sanfratello@ambrogio.com>
2015-08-27 12:02:48 +0200

    added -commit option to update_compiler.sh

--- update_compiler.sh
+++ update_compiler.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
 # curl should fail the pipe
+
 set -o pipefail
-
-LIST_URL="https://github.com/google/closure-compiler/wiki/Binary-Downloads"
-version_pattern='closure-compiler/compiler-([0-9]*)\.tar\.gz'
-
-if [[ "$(curl "${LIST_URL}")" =~ ${version_pattern} ]]; then
-  LATEST_VERSION=${BASH_REMATCH[1]}
-fi
-
-if [ "$?" != 0 ] || [ "" == "${LATEST_VERSION}" ]; then
-    echo "Failed to fetch or parse the version list from ${LIST_URL}"
-    exit 1
-fi
-echo "Found v${LATEST_VERSION}..."
 
 TMP_FILE="compiler.tar.gz"
 if [ -e "${TMP_FILE}" ]; then
@@ -28,7 +16,7 @@ if [ ! -d "${OUTPUT_DIR}" ]; then
   exit 1
 fi
 
-EXPECTED_URL="http://dl.google.com/closure-compiler/compiler-${LATEST_VERSION}.tar.gz"
+EXPECTED_URL="http://dl.google.com/closure-compiler/compiler-latest.tar.gz"
 if ! curl "${EXPECTED_URL}" > "${TMP_FILE}"; then
   echo "Failed to retrieve jar from ${EXPECTED_URL}..."
   rm "${TMP_FILE}"
@@ -46,10 +34,25 @@ rm "${TMP_FILE}"
 mv "tmp/compiler.jar" "${OUTPUT_DIR}"
 rm -rf tmp/
 
-MESSAGE="Updated compiler.jar to v${LATEST_VERSION}"
-git add "${OUTPUT_DIR}/compiler.jar"
-git commit -m "${MESSAGE}"
-git tag -a "closure-v${LATEST_VERSION}" -m "Updated closure to v${LATEST_VERSION}"
+if [ "$1" == "-commit" ]; then
+  LIST_URL="https://github.com/google/closure-compiler/wiki/Binary-Downloads"
+  version_pattern='closure-compiler/compiler-([0-9]*)\.tar\.gz'
+
+  if [[ "$(curl "${LIST_URL}")" =~ ${version_pattern} ]]; then
+    LATEST_VERSION=${BASH_REMATCH[1]}
+  fi
+
+  if [ "$?" != 0 ] || [ "" == "${LATEST_VERSION}" ]; then
+      echo "Failed to fetch or parse the version list from ${LIST_URL}"
+      exit 1
+  fi
+  echo "Found v${LATEST_VERSION}..."
+
+  MESSAGE="Updated compiler.jar to v${LATEST_VERSION}"
+  git add "${OUTPUT_DIR}/compiler.jar"
+  git commit -m "${MESSAGE}"
+  git tag -a "closure-v${LATEST_VERSION}" -m "Updated closure to v${LATEST_VERSION}"
+fi
 
 # This chews up the package.json formatting
 # npm version patch --message "Version %s -- v${LATEST_VERSION}"
